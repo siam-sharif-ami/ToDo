855c09788c9207f2cd02fc0288563419762bd991
Siam Sharif Ami <siam.sharif@brainstation-23.com>
2024-06-27 14:23:13 +0600

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by BS00484 on 27/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
